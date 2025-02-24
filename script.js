eb644ec7a248866655870fc046f7a88cdd092bd1
gippo05 <gsarinas17@gmail.com>
2025-02-24 17:17:03 +0800

    Made changes to Media Query
    Added tablet or ipad resolutions

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
 
+function checkRadio1(){
+    document.getElementsByClassName("radio1").checked = true;
+}
+
+function uncheckRadio1(){
+    document.getElementsByClassName("radio1").checked = true;
+}
+function checkRadio2(){
+    document.getElementsByClassName("radio2").checked = true;
+}
+
+function uncheckRadio2(){
+    document.getElementsByClassName("radio2").checked = true;
+}
+
+
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
