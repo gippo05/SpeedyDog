91842aca49c6bc43474aa32a873f7f6f309d1395
gippo05 <gsarinas17@gmail.com>
2025-02-26 09:08:33 +0800

    Quick Changes
    Added css layout on background image and modified some width

--- script.js
+++ script.js
@@ -5,21 +5,4 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
 
-function checkRadio1(){
-    document.getElementsByClassName("radio1").checked = true;
-}
-
-function uncheckRadio1(){
-    document.getElementsByClassName("radio1").checked = true;
-}
-function checkRadio2(){
-    document.getElementsByClassName("radio2").checked = true;
-}
-
-function uncheckRadio2(){
-    document.getElementsByClassName("radio2").checked = true;
-}
-
-
-
 document.getElementById("year").innerHTML = new Date().getFullYear();
